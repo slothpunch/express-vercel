4da95083e22f559aa7caa7409d662e3081d23d17
Kim <giminsub1@gmail.com>
2022-11-07 14:32:44 +1100

    update

--- index.js
+++ index.js
@@ -4,14 +4,13 @@
 const express = require('express');
 const app = express();
 
-// app.use(express.static('public')) // As it is a middleware, it should be above all request handlers 
+app.use(express.static('public')) // As it is a middleware, it should be above all request handlers 
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
   })
 
-// app.listen(process.env.PORT || 3000);
-
+app.listen(process.env.PORT || 3000);
 
 // Export the Express API
 module.exports = app 
