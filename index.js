864d404c9b8323c91f75e74b9767534cf370872d
Kim <giminsub1@gmail.com>
2022-11-07 14:08:04 +1100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public')) // As it is a middleware, it should be above all request handlers 
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  })
+
+// app.listen(process.env.PORT || 3000);
+
+
+// Export the Express API
+module.exports = app 
