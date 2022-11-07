db6dc40516bc0e20487ab2e50c80ac3557335782
Kim <giminsub1@gmail.com>
2022-11-07 14:22:21 +1100

    test

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
+// https://syntackle.live/blog/how-to-create-and-deploy-an-express-js-app-to-vercel-ljgvGrsCH7ioHsAxuw3G/
+
 const express = require('express');
 const app = express();
 
-app.use(express.static('public')) // As it is a middleware, it should be above all request handlers 
+// app.use(express.static('public')) // As it is a middleware, it should be above all request handlers 
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
